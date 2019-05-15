249fc25654d341b1153468741d3fe3dffbdb6679
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-15 21:52:55 +0530

    vue fix

--- resources/js/components/Chat.vue
+++ resources/js/components/Chat.vue
@@ -1,3 +1,35 @@
+<style scoped>
+    .panel-block {
+        flex-direction: column;
+        border: none;
+    }
+    .chat {
+        width: 100%;
+        padding: 20px;
+        box-shadow: inset 0 0 20px 0 grey;
+        margin-bottom: 20px;
+        border: 1px solid grey;
+        max-height: 600px;
+        overflow-x: auto;
+    }
+    .chat .chat-right, .chat .chat-left {
+        max-width: 70%;
+        box-shadow: 0 0 8px 0px grey;
+        padding: 8px;
+        margin: 4px;
+    }
+    .chat-right {
+        float: right;
+    }
+    .chat-left {
+        float: left;
+    }
+    .no-message {
+        height: 200px;
+        display: flex;
+        align-items: center;
+    }
+</style>
 <template>
     <div class="panel-block">
         <div class="chat" v-if="chats.length != 0">
@@ -10,7 +42,7 @@
                     </div>
                 </div>
         </div>
-        <div v-else>
+        <div v-else class="no-message">
             There are no messages
         </div>
 
@@ -20,6 +52,6 @@
 <script>
     export default {
         props: ['chats','userid','friendid'],
-       
+
     }
 </script>
