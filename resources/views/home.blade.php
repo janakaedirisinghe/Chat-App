9641e23da230812e77c3b9ba98f3c5182aed106a
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-16 18:05:52 +0530

    profile pic feature

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <div class="container">
+
         <div class="columns is-marginless is-centered">
             <div class="column is-7">
                 <nav class="card">
-                    <header class="card-header">
-                        <p class="card-header-title">
-                            Dashboard
-                        </p>
-                    </header>
+{{--                    <header class="card-header">--}}
+{{--                        <p class="card-header-title">--}}
+{{--                            Dashboard--}}
+{{--                        </p>--}}
+{{--                    </header>--}}
 
-                    <div class="card-content">
-                        You are logged in!
+{{--                    <div class="card-content">--}}
+{{--                        You are logged in!--}}
+{{--                    </div>--}}
+                    <div class="tabs is-toggle is-fullwidth is-large">
+                        <ul>
+                            <li >
+                                <a href="/chat">
+                                    <span class="icon"><i class="fa fa-user-circle" aria-hidden="true"></i></span>
+                                    <span>Friends</span>
+                                </a>
+                            </li>
+                            <br>
+                            <li>
+                                <a href="/users">
+                                    <span class="icon"><i class="fa fa-user-plus" aria-hidden="true"></i></span>
+                                    <span>Add Friend</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/chat">
+                                    <span class="icon"><i class="fa fa-mail-forward" aria-hidden="true"></i></span>
+                                    <span>Chat</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/account">
+                                    <span class="icon"><i class="fa fa-edit" aria-hidden="true"></i></span>
+                                    <span>My Account</span>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </nav>
             </div>
